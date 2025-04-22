0c4621fea67770e31354479ec57708ab2062877c
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-04-22 09:05:45 +0530

    Feat: Add `Book` object constructor

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const myLibrary = [];
+
+/*******************************************************************************************
+ * Object Constructor: Create instances of Books.                                          *
+ *******************************************************************************************/
+
+function Book(author, title, totalPages, readStatus) {
+  this.author = author;
+  this.title = title;
+  this.totalPages = totalPages;
+  this.readStatus = readStatus;
+}
+
+/*******************************************************************************************
+ * Helper Function: Takes in the params for "Books" and creates instances & stores it.     *
+ *******************************************************************************************/
+
+function addBookToLibrary() {}
